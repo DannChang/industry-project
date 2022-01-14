b393846cb0af462a67dba9b93f41b6c9b983a55d
Jason Yu <yaouyu@me.com>
2022-01-13 20:48:29 -0800

    created basic sidebar

--- client/src/components/TutorialSideMenu/TutorialSideMenu.js
+++ client/src/components/TutorialSideMenu/TutorialSideMenu.js
@@ -1,34 +1,87 @@
 import "./TutorialSideMenu.scss";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../../assets/images/Logo/logo.svg";
+import Radiobutton from "../../assets/images/Icons/radio-unselected.svg";
+import dots from "../../assets/images/Icons/connectingdots.svg";
 
 export default function TutorialSideMenu(props) {
-
-
-
-
-
-
-
-
-    return (
-        <>
-            <section className="side-menu">
-                <div>
-                    <div className="side-menu__welcome-container">
-                        <h1>Welcome</h1>
-                    </div>
-                    <div className="side-menu__basics-container">
-                        <h1>LEARN THE BASICS</h1>
-                        
-                    </div>
-                    <div className="side-menu__onboarding-container">
-
-                    </div>
-                </div>
-            </section>
-        </>
-
-        );
-
-
-
+  return (
+    <>
+      <section className="side-menu">
+        <div>
+          <Link to="/">
+            <img className="side-menu__logo" src={Logo} alt="Logo" />
+          </Link>
+          <div className="side-menu__welcome-container">
+            <h1 className="side-menu__title">Welcome!</h1>
+          </div>
+          <div className="side-menu__basics-container">
+            <h1 className="side-menu__title">LEARN THE BASICS</h1>
+            <Link className="side-menu__link-basics" to="/">
+              <img
+                className="side-menu__link-basics"
+                src={Radiobutton}
+                alt="Radiobutton"
+              />
+              <h4>Moving around</h4>
+            </Link>
+            <Link className="side-menu__link-basics" to="/">
+              <img
+                className="side-menu__link-basics"
+                src={Radiobutton}
+                alt="Radiobutton"
+              />
+              <h4>Interact with others</h4>
+            </Link>
+            <Link className="side-menu__link-basics" to="/">
+              <img
+                className="side-menu__link-basics"
+                src={Radiobutton}
+                alt="Radiobutton"
+              />
+              <h4>Play mini game</h4>
+            </Link>
+          </div>
+          <div className="side-menu__onboarding-container">
+            <h1 className="side-menu__title">ONBOARDING</h1>
+            <Link className="side-menu__link-onboarding" to="/">
+              <img
+                className="side-menu__link-onboarding"
+                src={Radiobutton}
+                alt="Radiobutton"
+              />
+              <h4>Workplace Culture</h4>
+            </Link>
+            <img className="dots" src={dots} />
+            <Link className="side-menu__link-onboarding" to="/">
+              <img
+                className="side-menu__link-onboarding"
+                src={Radiobutton}
+                alt="Radiobutton"
+              />
+              <h4>Mission & Values</h4>
+            </Link>
+            <img className="dots" src={dots} />
+            <Link className="side-menu__link-onboarding" to="/">
+              <img
+                className="side-menu__link-onboarding"
+                src={Radiobutton}
+                alt="Radiobutton"
+              />
+              <h4>Roles & Responsibilities</h4>
+            </Link>
+            <img className="dots" src={dots} />
+            <Link className="side-menu__link-onboarding" to="/">
+              <img
+                className="side-menu__link-onboarding"
+                src={Radiobutton}
+                alt="Radiobutton"
+              />
+              <h4>Health & Safety</h4>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- client/src/pages/TutorialPage/TutorialPage.js
+++ client/src/pages/TutorialPage/TutorialPage.js
@@ -2,32 +2,17 @@ import "./TutorialPage.scss";
 import TutorialSideMenu from "../../components/TutorialSideMenu/TutorialSideMenu";
 import TutorialEntry from "../../components/TutorialEntry/TutorialEntry";
 
-
 export default function TutorialPage(props) {
-
-
-
-
-
-
-
-
-    return (
-        <>
-            <section className="tutorial">
-                <article className="tutorial__side-menu">
-                    <TutorialSideMenu />
-
-                </article>
-                <article className="tutorial__main">
-                    <TutorialEntry />
-
-                </article>
-              
-        </section>
-        </>
-      );
-
-
-
+  return (
+    <>
+      <section className="tutorial">
+        <article className="tutorial__side-menu">
+          <TutorialSideMenu />
+        </article>
+        <article className="tutorial__main">
+          <TutorialEntry />
+        </article>
+      </section>
+    </>
+  );
 }
